9361f8cc508af16b2f983d94f8b5e2963187852b
Salomon van Huyssteen <salomonvh@gmail.com>
2022-05-30 13:27:46 +0200

    Failing testcase

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexViewTests(TestCase):
+    def test_index(self):
+        """
+        Test the index page.
+        """
+        response = self.client.get(reverse('polls:index'))
+        assert response.status_code == 200
