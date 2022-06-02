eab081b86c87a1b5d653b9afe2414e1711579097
Salomon van Huyssteen <salomonvh@gmail.com>
2022-06-02 16:20:14 +0200

    Modified unit test for nomigrations example.

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -7,12 +8,14 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    published_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
--- polls/tests.py
+++ polls/tests.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
@@ -23,6 +24,9 @@ class QuestionModelTests(TestCase):
         was_published_recently() returns False for questions whose pub_date
         is in the future.
         """
+        user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
         time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
+        future_question = Question(pub_date=time, published_by=user)
+        future_question.save()
         self.assertIs(future_question.was_published_recently(), False)
+        self.assertEqual(future_question.published_by, user)
