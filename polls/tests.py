debf1ccb96c7fc1a70b8910d00327297aeed1a4c
Salomon van Huyssteen <salomonvh@gmail.com>
2022-06-02 11:48:06 +0200

    Part 5: Was published test that failure

--- polls/tests.py
+++ polls/tests.py
@@ -1,5 +1,10 @@
+import datetime
+
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
+
+from .models import Question
 
 
 class IndexViewTests(TestCase):
@@ -9,3 +14,15 @@ class IndexViewTests(TestCase):
         """
         response = self.client.get(reverse('polls:index'))
         assert response.status_code == 200
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
