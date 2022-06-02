6024d9b4d24d40a07358ad95f9ada45d4a7e8090
Salomon van Huyssteen <salomonvh@gmail.com>
2022-06-02 11:08:55 +0200

    Part 1

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    response_text = "Hello, world. You're at the polls index."
+    return HttpResponse(response_text)
